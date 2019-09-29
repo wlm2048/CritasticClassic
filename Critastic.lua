1e17b7b3677582c4eef33af9a330747096dbf0c4
Bill Martin <wlm2048@gmail.com>
2019-09-29 15:48:24 -0400

    moved crits to categories, limited output to 3, added channel select

--- Critastic.lua
+++ Critastic.lua
@@ -13,8 +13,11 @@ local localdb = {}
 
 local defaults = {
   debug = 0,
-	output = "chatframe",
-	highscores = {}
+	output = "print",
+	highscores = {
+    ["harming"] = {},
+    ["healing"] = {}
+  }
 }
 
 function getPIBG()
@@ -66,15 +69,54 @@ function Critastic_OnLoad()
   SLASH_Critastic2 = "/crit"
 end
 
+function table.removeKey(t, k)
+	local i = 0
+	local keys, values = {},{}
+	for k,v in pairs(t) do
+		i = i + 1
+		keys[i] = k
+		values[i] = v
+	end
+
+	while i>0 do
+		if keys[i] == k then
+			table.remove(keys, i)
+			table.remove(values, i)
+			break
+		end
+		i = i - 1
+	end
+
+	local a = {}
+	for i = 1,#keys do
+		a[keys[i]] = values[i]
+	end
+
+	return a
+end
 
 function load_saved_data(...)
 	CritasticStats = copyDefaults(defaults, CritasticStats)
   -- update old true|false to current level based debug
+  if (CritasticStats["output"] == "chatframe") then
+    CritasticStats["output"] = "print"
+  end
   if (type(CritasticStats["debug"]) == "boolean") then
     if CritasticStats["debug"] then
       CritasticStats["debug"] = 1
     else
       CritasticStats["debug"] = 0
+    end
+  end
+  -- move old crits to the new categories
+  for action, max in pairs(CritasticStats["highscores"]) do
+    if not (action == "harming" or action == "healing") then
+      if (action == "Holy Light" or action == "Flash of Light") then
+        CritasticStats["highscores"]["healing"][action] = max
+      else
+        CritasticStats["highscores"]["harming"][action] = max
+      end
+      CritasticStats["highscores"] = table.removeKey(CritasticStats["highscores"], action)
     end
   end
 end
@@ -100,6 +142,7 @@ function cleu(event, ...)
   if CritasticStats["debug"] >= 3 then
     print(...)
   end
+  local type = "harming"
 	local spellId, spellName, spellSchool
 	local amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand
 	if subevent == "SWING_DAMAGE" then
@@ -108,27 +151,74 @@ function cleu(event, ...)
 		spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical, glancing, crushing, isOffHand = select(12, ...)
   elseif subevent == "SPELL_HEAL" then
     spellId, spellName, spellSchool, amount, _, _, critical = select(12, ...)
+    type = "healing"
 	end
 
 if critical and sourceGUID == playerGUID then
 		local action = spellName or "melee swing"
 		firstcrit = ""
     lastcrit = 0
-		if not CritasticStats["highscores"][action] or CritasticStats["highscores"][action] == 0 then
-			CritasticStats["highscores"][action] = 0
+		if not CritasticStats["highscores"][type][action] or CritasticStats["highscores"][type][action] == 0 then
+			CritasticStats["highscores"][type][action] = 0
 		else
-			firstcrit = MSG_CRITICAL_HIT_BEST:format(playerInfo["sex"], CritasticStats["highscores"][action])
-      lastcrit = CritasticStats["highscores"][action]
+			firstcrit = MSG_CRITICAL_HIT_BEST:format(playerInfo["sex"], CritasticStats["highscores"][type][action])
+      lastcrit = CritasticStats["highscores"][type][action]
 		end
 
-		if (amount > CritasticStats["highscores"][action]) then
+		if (amount > CritasticStats["highscores"][type][action]) then
 			critMessage = MSG_CRITICAL_HIT:format(playerInfo["name"], action, destName, amount) .. firstcrit
-			SendChatMessage(critMessage, "CHANNEL", "COMMON", channelID)
-			CritasticStats["highscores"][action] = amount
+			showOutput(critMessage)
+			CritasticStats["highscores"][type][action] = amount
     elseif CritasticStats["debug"] >= 2 then
       print("Already got " .. action .. ". last: " .. lastcrit .. " just now: " .. amount)
 		end
 	end
+end
+
+function table.slice(tbl, first, last, step)
+  local sliced = {}
+
+  for i = first or 1, last or #tbl, step or 1 do
+    sliced[#sliced+1] = tbl[i]
+  end
+
+  return sliced
+end
+
+function getKeysSortedByValue(...)
+  local tbl, sortFunction, max = ...
+  local keys = {}
+  for key in pairs(tbl) do
+    table.insert(keys, key)
+  end
+
+  table.sort(keys, function(a, b)
+    return sortFunction(tbl[a], tbl[b])
+  end)
+
+  if (max) then
+    keys = table.slice(keys, 1, max, 1)
+  end
+
+  return keys
+end
+
+function showOutput(data)
+  if (CritasticStats["output"] ~= "nvwow") then
+    print(format("%s", data))
+  else
+    SendChatMessage(format("%s", data), "CHANNEL", "COMMON", channelID)
+  end
+end
+
+function tableLength(t)
+  local count = 0
+  for _ in pairs(t) do count = count + 1 end
+  return count
+end
+
+function firstToUpper(str)
+    return (str:gsub("^%l", string.upper))
 end
 
 function Critastic_SlashCrit(msg)
@@ -139,15 +229,27 @@ function Critastic_SlashCrit(msg)
 		param = ""
 	end
   if cmd == "show" then
-    SendChatMessage("Max crits for " .. playerInfo["name"] .. ":", "CHANNEL", "COMMON", channelID)
-    for action, max in pairs(CritasticStats["highscores"]) do
-      SendChatMessage(action .. ": " .. max, "CHANNEL", "COMMON", channelID)
+    local show_top = 3
+    if (param == "all") then show_top = nil end
+    showOutput("Max crits for " .. playerInfo["name"] .. ":")
+    local types = {"harming", "healing"}
+    for _, type in ipairs(types) do
+      if (tableLength(CritasticStats["highscores"][type]) > 0) then
+        showOutput(format(" %s:", firstToUpper(type)))
+        local sorted_keys = getKeysSortedByValue(CritasticStats["highscores"][type], function(a, b) return a > b end, show_top)
+        for _, key in ipairs(sorted_keys) do
+          showOutput(format("  %s: %d", key, CritasticStats["highscores"][type][key]))
+        end
+      end
     end
   elseif cmd == "reset" and param ~= "really" then
     print("Run '" .. slashcmd .. " reset really' to actually reset your scores.")
   elseif cmd == "reset" and param == "really" then
     print("Resetting crits for " .. playerInfo["name"])
-    CritasticStats["highscores"] = {}
+    CritasticStats["highscores"] = {
+      ["harming"] = {},
+      ["healing"] = {}
+    }
   elseif cmd == "debug" then
     if param == "1" or param == "2" or param == "3" then
       CritasticStats["debug"] = tonumber(param)
@@ -160,6 +262,13 @@ function Critastic_SlashCrit(msg)
     else
       print("debug " .. param .. " not understood, use debug [ 0 | 1 | 2 | 3 ]")
     end
+  elseif cmd == "channel" then
+      if param == "nvwow" then
+        CritasticStats["output"] = "nvwow"
+      else
+        CritasticStats["output"] = "print"
+      end
+      print(format("%s output set to %s", AddonName, CritasticStats["output"]))
   else
     print("end of cmd check: " .. cmd)
   end
